16d17de2a85d53052eb997104b75b25a1b11ec2a
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2020-08-26 16:38:47 -0500

    Cleaned up code. :gear:

--- src/figure.py
+++ src/figure.py
@@ -37,44 +37,44 @@ def plotly_figure_1(data):
 def plotly_figure_2(data):
     # Iris-setosa
     trace_setosa = go.Scatter3d(
-                            x = data['sepal length (cm)'][:50],
-                            y = data['sepal width (cm)'][:50],
-                            z = data['petal length (cm)'][:50],
-                            mode = 'markers',
-                            opacity = 0.7,
-                            name = "Iris-setosa",
-                            marker = dict(
-                                        size = 5,
-                                        color = 'rgba(255,102, 255,0.8)'
-                            )
+        x = data['sepal length (cm)'][:50],
+        y = data['sepal width (cm)'][:50],
+        z = data['petal length (cm)'][:50],
+        mode = 'markers',
+        opacity = 0.7,
+        name = "Iris-setosa",
+        marker = dict(
+                    size = 5,
+                    color = 'rgba(255,102, 255,0.8)'
+        )
     )
 
     # Iris-versicolor
     trace_versicolor = go.Scatter3d(
-                            x = data['sepal length (cm)'][50:100],
-                            y = data['sepal width (cm)'][50:100],
-                            z = data['petal length (cm)'][50:100],
-                            mode = 'markers',
-                            opacity = 0.7,
-                            name = "Iris-versicolor",
-                            marker = dict(
-                                        size = 5,
-                                        color = 'rgba(102, 255, 51, 0.8)'
-                            )
+        x = data['sepal length (cm)'][50:100],
+        y = data['sepal width (cm)'][50:100],
+        z = data['petal length (cm)'][50:100],
+        mode = 'markers',
+        opacity = 0.7,
+        name = "Iris-versicolor",
+        marker = dict(
+                    size = 5,
+                    color = 'rgba(102, 255, 51, 0.8)'
+        )
     )
 
     # Iris-virginica
     trace_virginica = go.Scatter3d(
-                            x = data['sepal length (cm)'][100:],
-                            y = data['sepal width (cm)'][100:],
-                            z = data['petal length (cm)'][100:],
-                            mode = 'markers',
-                            opacity = 0.7,
-                            name = "Iris-virginica",
-                            marker = dict(
-                                        size = 5,
-                                        color = 'rgba(51, 102, 255, 0.8)'
-                            )
+        x = data['sepal length (cm)'][100:],
+        y = data['sepal width (cm)'][100:],
+        z = data['petal length (cm)'][100:],
+        mode = 'markers',
+        opacity = 0.7,
+        name = "Iris-virginica",
+        marker = dict(
+                    size = 5,
+                    color = 'rgba(51, 102, 255, 0.8)'
+        )
     )
 
     list_3d = [trace_setosa, trace_versicolor, trace_virginica]
