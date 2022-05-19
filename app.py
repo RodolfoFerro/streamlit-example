3fdca1294353adfbe6a93433021b27e39d829d9f
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2022-05-19 03:06:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # ==============================================================
 # Author: Rodolfo Ferro
-# Twitter: @FerroRodolfo
+# Twitter: @rodo_ferro
 #
 # ABOUT COPYING OR USING PARTIAL INFORMATION:
 # This script has been originally created by Rodolfo Ferro.
